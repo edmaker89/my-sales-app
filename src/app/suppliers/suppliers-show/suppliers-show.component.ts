af1dd470812984cd1cc02298678ad8c6aacb27d1
edmaker <edmaker@gmail.com>
2024-05-09 20:50:53 -0300

    exibir detalhes do fornecedor

--- src/app/suppliers/suppliers-show/suppliers-show.component.ts
+++ src/app/suppliers/suppliers-show/suppliers-show.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MaterialModule } from '../../material.module';
+import { AsyncPipe } from '@angular/common';
+import { LoadingBarComponent } from '../../loading-bar.component';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { SupplierService } from '../supplier.service';
+import { Suppliers } from '../suppliers.dto';
+import { Observable, lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-suppliers-show',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, AsyncPipe, LoadingBarComponent, RouterLink],
   templateUrl: './suppliers-show.component.html',
   styles: ``
 })
-export class SuppliersShowComponent {
+export class SuppliersShowComponent implements OnInit {
+  
+  route = inject(ActivatedRoute)
+  supplierService = inject(SupplierService)
+  supplier: Suppliers
+  supplierObservable: Observable<Suppliers>
+  
+  async ngOnInit(): Promise<void> {
+    const id: Number = +(this.route.snapshot.paramMap.get('id') || 0 );
+    this.supplierObservable = this.supplierService.getByid(id)
+    this.supplier = await lastValueFrom(this.supplierObservable)
+  }
 
 }
